1c0f438148acb27effa53c346d4b5f40bd01f9d6
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-19 15:45:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 function showRepositories() {
-  const repos = JSON.parse(this.responseText)
-  const src = document.getElementById('repository-template').innerHTML
-  const template = Handlebars.compile(src)
-  const repoList = template(repos)
-  document.getElementById('repositories').innerHTML = repoList
+  const repos = JSON.parse(this.responseText);
+  const src = document.getElementById("repository-template").innerHTML;
+  const template = Handlebars.compile(src);
+  const repoList = template(repos);
+  document.getElementById("repositories").innerHTML = repoList;
 }
 
 function getRepositories() {
-  const req = new XMLHttpRequest()
-  req.addEventListener('load', showRepositories)
-  req.open('GET', 'https://api.github.com/users/tuzmusic/repos')
-  req.send()
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", showRepositories);
+  req.open("GET", "https://api.github.com/users/tuzmusic/repos");
+  req.send();
 }
 
-document.addEventListener('DOMContentLoaded', (event) => {
-  Handlebars.registerPartial('authorPartial', document.getElementById('author-partial-template').innerHTML)
-})
+document.addEventListener("DOMContentLoaded", event => {
+  Handlebars.registerPartial(
+    "authorPartial",
+    document.getElementById("author-partial-template").innerHTML
+  );
+});
